ae5d65d22f257f6b2dc882b252c591d931ab3b43
natalilazaric <nl53743@fer.hr>
2025-11-05 12:38:58 +0100

    popravak

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); 
 
 let insecureStorage = true;
 
