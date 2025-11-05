8979e60a751cc7354c74493372e8953894111a19
natalilazaric <nl53743@fer.hr>
2025-11-05 12:29:50 +0100

    render url

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgres://postgres:lozinka123@db.jhmpqblmtvgqrixtyhmg.supabase.co:5432/postgres',
-  ssl: { rejectUnauthorized: false }  // Supabase zahtijeva SSL
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Supabase zahtijeva SSL
 });
 
 module.exports = pool;
